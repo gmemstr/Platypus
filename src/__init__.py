537d70c5472c0ee4faa727661046c7740a5a7348
Gabriel Simmer <Gabriel Simmer>
2016-09-26 16:50:54 -0700

    *sigh* Fixing port issue (another commit may shortly follow)

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 import Scan
 app = Flask(__name__)
 
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
+
 @app.route('/')
 def checkall():
 	return render_template("list.html", location = "worldwide", downs = Scan.cache("all"))
