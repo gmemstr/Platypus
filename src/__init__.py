decd28180770863e51d267bbfe49d809dc5a601c
Gabriel Simmer <Gabriel Simmer>
2016-09-26 16:56:01 -0700

    Forgot to import 'os'

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 import Scan
+from os import environ
 app = Flask(__name__)
 
-port = int(os.environ.get('PORT', 5000))
+port = int(environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
 
 @app.route('/')
