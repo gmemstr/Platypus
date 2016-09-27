6ee6d4931d067ab4a5453147f4c889dd3c33cb07
Gabriel Simmer <Gabriel Simmer>
2016-09-26 17:19:24 -0700

    Moved variable

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,8 @@ import Scan
 from os import environ
 app = Flask(__name__)
 
+port = int(environ.get('PORT', 5000))
+
 @app.route('/')
 def checkall():
 	return render_template("list.html", location = "worldwide", downs = Scan.cache("all"))
@@ -17,5 +19,4 @@ def forcescan():
 	return "<script>window.location.href = '/';</script>"
 	
 if __name__ == '__main__':
-   port = int(environ.get('PORT', 5000))
    app.run(host='0.0.0.0', port=port)
