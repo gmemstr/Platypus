8dcc61e3db14ec062a4f33d4b1821f999539dc20
Gabriel Simmer <Gabriel Simmer>
2016-09-26 17:24:31 -0700

    Given up most hope

--- src/__init__.py
+++ src/__init__.py
@@ -3,8 +3,6 @@ import Scan
 from os import environ
 app = Flask(__name__)
 
-port = int(environ.get('PORT', 5000))
-
 @app.route('/')
 def checkall():
 	return render_template("list.html", location = "worldwide", downs = Scan.cache("all"))
@@ -17,6 +15,6 @@ def checkone(lctn):
 def forcescan():
 	Scan.updatecache()
 	return "<script>window.location.href = '/';</script>"
-	
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=port)
+
+port = int(environ.get('PORT', 5000))	
+app.run(host='0.0.0.0', port=port)
