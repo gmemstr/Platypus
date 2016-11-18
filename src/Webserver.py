80bf6c83538e8915444d926933c2181e6f0a0e98
Gabriel Simmer <bladesimmer@gmail.com>
2016-11-18 11:58:03 -0800

    Some tweaks and FS cleanup.

--- src/Webserver.py
+++ src/Webserver.py
@@ -1,28 +1,32 @@
 from flask import Flask, render_template, send_from_directory, jsonify
 import Scan
 
-from os import environ
 app = Flask(__name__)
+
 
 @app.route('/')
 def checkall():
-	return render_template("list.html", location = "worldwide", downs = Scan.cache("all"))
-	
+    return render_template("list.html", location="worldwide", downs=Scan.cache("all"))
+
+
 @app.route('/blob/<lctn>')
 def checkone(lctn):
-	return render_template("list.html", location = lctn, downs = Scan.cache(lctn))
+    return render_template("list.html", location=lctn, downs=Scan.cache(lctn))
+
 
 @app.route('/forcescan')
 def forcescan():
-	Scan.updatecache()
-	return "<script>window.location.href = '/';</script>"
+    Scan.updatecache()
+    return "<script>window.location.href = '/';</script>"
+
 
 @app.route('/raw')
 def rawjson():
-	file = open("cache.json", "r").read()
-	return file
-	
+    file = open("cache.json", "r").read()
+    return file
+
 if __name__ == '__main__':
 
-	app.run()
-	# app.run(host='0.0.0.0') # Make server purblicly visible
+    app.run()
+    # app.run(host='0.0.0.0') # Make server purblicly visible
+	
