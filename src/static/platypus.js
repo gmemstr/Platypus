1784ac6c1764e0476832eccd95f41c4690d60452
gmemstr <bladesimmer@gmail.com>
2017-02-27 10:29:43 -0800

    Small bugfix to refreshing stats
    Index was not reset properly when it reached the end of the table. RTS are slow but
    it may be possible to implement websockets to them.

--- src/static/platypus.js
+++ src/static/platypus.js
@@ -1,40 +1,31 @@
 window.onload = checkNight;
 // Function that refreshes each
 // panels stats indivudually
-setInterval(RequestStat, 2000);
-i = 0;
+setInterval(RequestStat, 1000);
 var table = document.getElementById("table");
+var i=0;
 
 function RequestStat() {
     if (document.getElementById("rts").checked == true) {
-        var row = table.rows[i];
-        var panel = row.id;
-        Request("/fetch/" + panel, function(text) {
-            res = JSON.parse(text);
-            if (res[panel]['online'] == 0) {
-                row.cells[0].innerHTML = res[panel]['name'] + "<strong>OFFLINE</strong>";
-                row.cells[2].innerHTML = "0%";
-                row.cells[3].innerHTML = "0%";
-                row.cells[4].innerHTML = "0%";
-            } else {
-                row.cells[0].innerHTML = res[panel]['name'];
-                row.cells[2].innerHTML = res[panel]['cpu'] + "%";
-                row.cells[3].innerHTML = res[panel]['memory'] + "%";
-                row.cells[4].innerHTML = res[panel]['disk'] + "%";
-            }
-        });
-        if (i > table.rows.length){ i=0; }else{ i++; }
-    }
+            if (i < table.rows.length){ i++; }
+            else { i = 0; }
+            //console.log(i);
+            var row = table.rows[i];
+            var panel = row.id;
+            //console.log(row);
+            Request(panel, setRow, "GET", row);
+        }
 }
 
-function Request(url, callback, method = "GET") {
+function Request(panel, callback, method = "GET", row) {
+    url = "/fetch/"+panel;
     var request = new XMLHttpRequest();
     request.open(method, url);
     request.send();
 
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200)
-            callback(request.responseText);
+            callback(request.responseText,panel,row);
     }
 }
 
@@ -43,6 +34,23 @@ function toggleRts() {
         localStorage.setItem("rts", true);
     } else {
         localStorage.setItem("rts", false);
+    }
+}
+
+function setRow(text,panel,row){
+    //console.log(text);
+    //console.log(panel)
+    res = JSON.parse(text);
+    if (res[panel]['online'] == 0 || res[panel]['online'] == null) {
+        row.cells[0].innerHTML = res[panel]['name'] + "<strong>OFFLINE</strong>";
+        row.cells[2].innerHTML = "0%";
+        row.cells[3].innerHTML = "0%";
+        row.cells[4].innerHTML = "0%";
+    } else {
+        row.cells[0].innerHTML = res[panel]['name'];
+        row.cells[2].innerHTML = res[panel]['cpu'] + "%";
+        row.cells[3].innerHTML = res[panel]['memory'] + "%";
+        row.cells[4].innerHTML = res[panel]['disk'] + "%";
     }
 }
 
