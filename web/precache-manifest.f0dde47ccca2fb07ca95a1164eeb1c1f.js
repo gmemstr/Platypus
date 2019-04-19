d3c04a1392610f58f6fc7d79103ec9768a060918
Gabriel Simmer <gsimmer@acromedia.com>
2019-04-19 15:18:47 -0700

    Clean up web dir.

---
+++ web/precache-manifest.f0dde47ccca2fb07ca95a1164eeb1c1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd5b86ce236c1c4592a3",
+    "url": "/static/css/main.0508ec29.chunk.css"
+  },
+  {
+    "revision": "dd5b86ce236c1c4592a3",
+    "url": "/static/js/main.cfe6a328.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "a4654cf03a62364f897a06ad9b277d04",
+    "url": "/index.html"
+  }
+];
